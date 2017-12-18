8c42c0cefbf60b3ed57bc74bd8d5edcb118871e9
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-12-18 15:08:42 +0800

    create workspace

---
+++ environment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Activating ROS..."
+source /opt/ros/kinetic/setup.bash
+echo "...done."
+
+echo "Setting up PYTHONPATH."
+export PYTHONPATH=/home/ubuntu/3D_Object_Modeling/catkin_ws/src:$PYTHONPATH
+
+echo "Setup ROS_HOSTNAME."
+export ROS_HOSTNAME=$HOSTNAME.local
+export MODELING_ROOT=$HOME/3D_Object_Modeling
+
+echo "Building machines file..."
+make -C  $MODELING_ROOT
+echo "...done"
+echo "Activating development."
+source $MODELING_ROOT/catkin_ws/devel/setup.bash
+source $MODELING_ROOT/set_ros_master.sh master
+
+# TODO: check that the time is >= 2015
+
+# TODO: run a python script that checks all libraries are installed
+
+exec "$@" #Passes arguments. Need this for ROS remote launching to work.
+
